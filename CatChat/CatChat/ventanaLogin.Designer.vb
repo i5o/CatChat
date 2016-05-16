9a9b4a701c1ffdced1cd87d7ebcc0d504e4e250f
Ignacio Rodríguez <ignacio@sugarlabs.org>
2016-05-16 04:52:37 -0300

    fondo

--- CatChat/CatChat/ventanaLogin.Designer.vb
+++ CatChat/CatChat/ventanaLogin.Designer.vb
@@ -28,6 +28,8 @@ Partial Class ventanaLogin
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.CatChat.My.Resources.Resources.Fondo
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(1254, 649)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
