2b07abf9b289202a2444fa59ad1c4018369ae72d
Ignacio Rodríguez <ignacio@sugarlabs.org>
2016-05-16 04:41:53 -0300

    ventanaLogin - creada

--- CatChat/CatChat/ventanaLogin.Designer.vb
+++ CatChat/CatChat/ventanaLogin.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class ventanaLogin
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
@@ -22,9 +22,20 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'ventanaLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(1254, 649)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "ventanaLogin"
+        Me.Text = "CatChat"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
